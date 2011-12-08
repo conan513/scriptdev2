561e08677081d656121b8a17f141212f7fbafc37
Sar <orionic.gm@gmail.com>
2011-12-08 17:06:26 +0300

    New script Valithria in Icecrown Citadel. Need Test

---
+++ sql_updates/wb_049_scriptdev2_icecrown_citadel.sql
@@ -0,0 +1,13 @@
+set names utf8;
+-- ---------------------
+-- Valithria dreamwalker
+-- ---------------------
+REPLACE INTO script_texts (entry, content_default, content_loc8, sound, type, language, emote, comment) VALUES
+(-1631140,'Heroes, lend me your aid! I... I cannot hold them off much longer! You must heal my wounds!','Герои! Вы должны мне помочь! Мои силы на исходе... Залечите мои раны...',17064,1,0,0,'dreamwalker SAY_AGGRO'),
+(-1631141,'I have opened a portal into the Dream. Your salvation lies within, heroes.','Я открыла портал в изумрудный сон. Там вы найдете спасение, герои!',17068,1,0,0,'dreamwalker SAY_PORTAL'),
+(-1631142,'My strength is returning! Press on, heroes!','Силы возвращаются ко мне! Герои, еще немного!',17070,1,0,0,'dreamwalker SAY_75_HEALTH'),
+(-1631143,'I will not last much longer!','Я долго не продержусь!',17069,1,0,0,'dreamwalker SAY_25_HEALTH'),
+(-1631144,'Forgive me for what I do! I... cannot... stop... ONLY NIGHTMARES REMAIN!','Простите меня, я не могу остано... ВСЕ ВО ВЛАСТИ КОШМАРА!',17072,1,0,0,'dreamwalker SAY_0_HEALTH'),
+(-1631145,'A tragic loss...','Прискобная потеря.',17066,1,0,0,'dreamwalker SAY_PLAYER_DIES'),
+(-1631146,'FAILURES!','Неудачники!',17067,1,0,0,'dreamwalker SAY_BERSERK'),
+(-1631147,'I am renewed! Ysera grants me the favor to lay these foul creatures to rest!','Я излечилась! Изера, даруй мне силу покончить с этими нечестивыми тварями!',17071,1,0,0,'dreamwalker SAY_VICTORY');
